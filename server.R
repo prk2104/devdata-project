b5eca3ef3b340887e23d15a19a64f2c5446a43f1
prk2104 <prk2104@users.noreply.github.com>
2016-03-06 17:22:37 -0500

    Added files via upload

---
+++ server.R
@@ -0,0 +1,28 @@
+library(shiny)
+library(datasets)
+
+
+mpgData <- mtcars
+mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+
+shinyServer(function(input, output) {
+  
+  # Compute the forumla text in a reactive expression since it is 
+  # shared by the output$caption and output$mpgPlot expressions
+  formulaText <- reactive({
+    paste("mpg ~", input$variable)
+  })
+  
+  # Return the formula text for printing as a caption
+  output$caption <- renderText({
+    formulaText()
+  })
+  
+  # Generate a plot of the requested variable against mpg and only 
+  # include outliers if requested
+  output$mpgPlot <- renderPlot({
+    boxplot(as.formula(formulaText()), 
+            data = mpgData,
+            outline = input$outliers)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+
+
+shinyUI(fluidPage(
+  
+  # title
+  headerPanel("Miles Per Gallon"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+    selectInput("variable", "Variable:",
+                list("Cylinders" = "cyl", 
+                     "Transmission" = "am", 
+                     "Gears" = "gear")),
+    
+    checkboxInput("outliers", "Show outliers", FALSE)
+  ),
+  
+  # Show the caption and plot of the requested variable against mpg
+  mainPanel(
+    h3(textOutput("caption")),
+    
+    plotOutput("mpgPlot")
+  )
+))
